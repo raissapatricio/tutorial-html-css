7e8442f212c1bcd6a2f0391a65249f5250beef75
Raissa Patricio <raissapatricio2020@gmail.com>
2024-11-07 21:55:09 -0300

    continuação da criação do modal

--- iffinance/js/main.js
+++ iffinance/js/main.js
@@ -1,3 +1,15 @@
-funtion openModal(){
+function openModal(){
+    const modal = document.querySelector('.modal')
+    console.log(modal)
+    modal.style.display = 'flex'
+}
+
+function closeModal(event){
+    const modal = document.querySelector('.modal')
     
+    if(event === undefined)
+        return modal.style.display = 'none'
+    
+    if(event.target.className === 'modal')
+        return modal.style.display = 'none'
 }
