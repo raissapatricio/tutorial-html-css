cc7847153fecd4996936a811c2eb648e0414b748
Raissa Patricio <raissapatricio2020@gmail.com>
2024-11-14 21:45:35 -0300

    aula 14/11/24

--- iffinance/js/main.js
+++ iffinance/js/main.js
@@ -1,15 +1,24 @@
 function openModal(){
     const modal = document.querySelector('.modal')
-    console.log(modal)
     modal.style.display = 'flex'
 }
 
 function closeModal(event){
     const modal = document.querySelector('.modal')
-    
+
     if(event === undefined)
         return modal.style.display = 'none'
-    
+
     if(event.target.className === 'modal')
         return modal.style.display = 'none'
+}
+
+function handleSubmitAddTicker(event){
+    //previne o comportamento padrão de recarregar a página ou enviar para algum endereço (action)
+    event.preventDefault()
+    console.log(event.target.urlLogo.value)
+    console.log(event.target.nameCompany.value)
+    console.log(event.target.ticker.value)
+    console.log(event.target.quantity.value)
+    console.log(event.target.closedValue.value)
 }
